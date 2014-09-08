7295f2db92ed3c58311e0e69f2511552c5d0d867
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 23:32:39 +0100

    Refactored the repositories service provider

--- app/Sightseeing/Repositories/SightseeingServiceProvider.php
+++ app/Sightseeing/Repositories/SightseeingServiceProvider.php
@@ -4,22 +4,27 @@ use Illuminate\Support\ServiceProvider;
 
 class SightseeingServiceProvider extends ServiceProvider {
 
+    protected $repositories = [
+        'City',
+        'Country',
+        'Sight',
+        'Beacon'
+    ];
+
     /**
-     * Register the service provider.
+     * Register the repositories service providers.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->bind(
-            'Sightseeing\Repositories\City\CityRepository',
-            'Sightseeing\Repositories\City\EloquentCityRepository'
-        );
-
-        $this->app->bind(
-            'Sightseeing\Repositories\Country\CountryRepository',
-            'Sightseeing\Repositories\Country\EloquentCountryRepository'
-        );
+        //Loops through all repositories and binds them with their Eloquent implementation
+        array_walk($this->repositories, function($repository) {
+            $this->app->bind(
+                'Sightseeing\Repositories\\' . $repository . '\\' . $repository . 'Repository',
+                'Sightseeing\Repositories\\' . $repository . '\\Eloquent' . $repository . 'Repository'
+            );
+        });
     }
 
 } 
