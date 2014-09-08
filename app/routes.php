63e7599f84e53ec8ddb1b041290fb79fb141c0cc
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 15:11:03 +0100

    Added test routes

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('api/v1/search', array('as' => 'companies.search', 'uses' => 'CompaniesController@search'));
 
-Route::get('/', function()
+Route::group(array('prefix' => 'api/v1'), function()
 {
-	return View::make('hello');
+
+    Route::get('cities', array('as' => 'cities.list', 'uses' => 'CitiesController@index'));
+
+});
+
+Route::get('/', function() {
+    dd('Under construction!');
 });
