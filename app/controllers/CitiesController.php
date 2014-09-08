512aae8346f42f3b5fbdd8ac2b7d771cbef68baf
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 15:10:50 +0100

    Added cities controller

---
+++ app/controllers/CitiesController.php
@@ -0,0 +1,27 @@
+<?php
+
+use League\Fractal\Manager;
+use Sightseeing\Repositories\City\CityRepository;
+use Sightseeing\Transformers\CityTransformer;
+
+class CitiesController extends ApiController {
+
+    /**
+     * @var CityRepository
+     */
+    private $cityRepository;
+
+    function __construct(CityRepository $cityRepository, Manager $manager)
+    {
+        parent::__construct($manager);
+        $this->cityRepository = $cityRepository;
+    }
+
+    public function index()
+    {
+        $cities = $this->cityRepository->getAll();
+
+        return $this->respondWithCollection($cities, new CityTransformer);
+    }
+
+} 
