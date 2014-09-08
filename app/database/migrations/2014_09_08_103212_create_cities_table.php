06ffce0bc6c5324b303d5492bf7a5070ee2afd17
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 16:13:09 +0100

    Added country id to cities table

--- app/database/migrations/2014_09_08_103212_create_cities_table.php
+++ app/database/migrations/2014_09_08_103212_create_cities_table.php
@@ -15,6 +15,8 @@ class CreateCitiesTable extends Migration {
         Schema::create('cities', function(Blueprint $table)
         {
             $table->increments('id');
+            $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')->references('id')->on('countries');
             $table->string('name');
             $table->string('slug');
             $table->string('timezone');
