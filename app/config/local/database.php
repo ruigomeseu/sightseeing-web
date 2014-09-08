44c798cecdb6aa0b722c4fe4d48ede20ca13f636
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 23:31:00 +0100

    Updated local config to use env file

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'sightseeing',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+            'host'      => $_ENV['MYSQL_HOST'],
+            'database'  => $_ENV['MYSQL_DATABASE'],
+            'username'  => $_ENV['MYSQL_USERNAME'],
+            'password'  => $_ENV['MYSQL_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
