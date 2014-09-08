f51e62d2ced64c478cddd4190654235c469123dd
Rui Gomes <ruigomeseu@gmail.com>
2014-09-08 15:08:21 +0100

    Updated Homestead DB

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
+			'database'  => 'sightseeing',
 			'username'  => 'homestead',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
