bb9850cd6b1e5c5cd122fa4188e2a20bf9ae8223
Rui Gomes <ruigomeseu@gmail.com>
2014-09-09 22:17:59 +0100

    Fixed landing page title

--- app/views/layout/landing/main.blade.php
+++ app/views/layout/landing/main.blade.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Error 404 - Pages - PixelAdmin</title>
+	<title>Sightseeing.io</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
 	<!-- Open Sans font from Google CDN -->
