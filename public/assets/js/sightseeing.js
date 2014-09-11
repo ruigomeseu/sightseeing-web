bd0626308d1d407946c6a0c75bf614d723a33908
Rui Gomes <ruigomeseu@gmail.com>
2014-09-12 00:04:12 +0100

    Added a JS pusher listener

---
+++ public/assets/js/sightseeing.js
@@ -0,0 +1,20 @@
+(function() {
+    var pusher = new Pusher('a82226d6fb9309a8cfe8');
+
+    var channel = pusher.subscribe('sightseeing.io');
+
+    window.App = {};
+
+    App.Notifier = function() {
+        this.notify = function(title, message) {
+            jQuery( document ).ready(function($) {
+                $.growl.notice({ title: title, message: message });
+            });
+        }
+    }
+
+    channel.bind('userCheckedIn', function(data) {
+        (new App.Notifier).notify("New visitor", "A new visitor just checked in at XXXXX");
+    });
+
+})();
