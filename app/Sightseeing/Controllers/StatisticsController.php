1bb603a0708aa74afe6c8f1a0c2f4a681fcfa776
Rui Gomes <ruigomeseu@gmail.com>
2014-09-12 21:19:39 +0100

    Created a statistics controller

---
+++ app/Sightseeing/Controllers/StatisticsController.php
@@ -0,0 +1,89 @@
+<?php namespace Sightseeing\Controllers;
+
+use Sightseeing\Repositories\Checkin\CheckinRepository;
+use View;
+
+class StatisticsController extends BaseController {
+
+    /**
+     * @var CheckinRepository
+     */
+    private $checkinRepository;
+
+    function __construct(CheckinRepository $checkinRepository)
+    {
+        $this->checkinRepository = $checkinRepository;
+    }
+
+    public function index()
+    {
+        $checkins = $this->checkinRepository->getAll(['sight']);
+
+        $visitsPerSight = array();
+        $visitsPerCountry = array();
+        $sightsPerVisitor = array();
+
+        foreach($checkins as $checkin)
+        {
+            //Visits per Sight
+            if( ! array_key_exists($checkin->sight->name, $visitsPerSight))
+            {
+                $visitsPerSight[$checkin->sight->name] = 0;
+            }
+            $visitsPerSight[$checkin->sight->name]++;
+
+
+            //Visits Per Country
+            if( ! array_key_exists($checkin->country, $visitsPerCountry))
+            {
+                $visitsPerCountry[$checkin->country] = 0;
+            }
+
+            $visitsPerCountry[$checkin->country]++;
+
+
+            //Sights Per Visitor
+            if ( ! array_key_exists($checkin->mac_address, $sightsPerVisitor))
+            {
+                $sightsPerVisitor[$checkin->mac_address] = 0;
+            }
+
+            $sightsPerVisitor[$checkin->mac_address]++;
+
+        }
+
+        $sightsPerVisitorNormalized = array();
+        $sightsPerVisitorNormalized[1] = 0;
+        $sightsPerVisitorNormalized[2] = 0;
+        $sightsPerVisitorNormalized[3] = 0;
+        $sightsPerVisitorNormalized[4] = 0;
+        $sightsPerVisitorNormalized[5] = 0;
+
+        foreach($sightsPerVisitor as $sights)
+        {
+            if($sights >= 5)
+            {
+                $sightsPerVisitorNormalized[5]++;
+            } elseif ($sights >= 4)
+            {
+                $sightsPerVisitorNormalized[4]++;
+            } elseif ($sights >= 3)
+            {
+                $sightsPerVisitorNormalized[3]++;
+            } elseif ($sights >= 2)
+            {
+                $sightsPerVisitorNormalized[2]++;
+            } elseif ($sights >= 1)
+            {
+                $sightsPerVisitorNormalized[1]++;
+            }
+        }
+
+        return View::make("statistics.index")
+            ->with('title', 'City Statistics')
+            ->with('visitsPerSight', $visitsPerSight)
+            ->with('visitsPerCountry', $visitsPerCountry)
+            ->with('sightsPerVisitor', $sightsPerVisitorNormalized);
+    }
+
+} 
