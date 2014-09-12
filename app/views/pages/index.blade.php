a17b48156e442f8aadb3308bc8e7f3cf80d23600
Rui Gomes <ruigomeseu@gmail.com>
2014-09-12 21:20:15 +0100

    Changed index to a static page

---
+++ app/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.landing.main')
+
+@section('content')
+
+<div class="error-code">Sightseeing.io</div>
+
+    <br />
+    <br />
+
+	<div class="error-text">
+	    <iframe width="853" height="480" src="//www.youtube.com/embed/vXkN9HyF0yY?rel=0" frameborder="0" allowfullscreen></iframe>
+	</div>
+
+@endsection
