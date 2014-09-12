2747abffbde156949fcbbb4eb254bb38c9829140
Rui Gomes <ruigomeseu@gmail.com>
2014-09-12 21:21:30 +0100

    Dynamic landing template

--- app/views/layout/landing/main.blade.php
+++ app/views/layout/landing/main.blade.php
@@ -27,14 +27,7 @@
 
 <script>var init = [];</script>
 
-	<div class="error-code">Sightseeing.io</div>
-
-    <br />
-    <br />
-
-	<div class="error-text">
-	    <iframe width="853" height="480" src="//www.youtube.com/embed/vXkN9HyF0yY?rel=0" frameborder="0" allowfullscreen></iframe>
-	</div> <!-- / .error-text -->
+	@yield('content')
 
 <!-- Get jQuery from Google CDN -->
 <!--[if !IE]> -->
