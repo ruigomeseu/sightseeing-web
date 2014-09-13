8c93c1fb77436c250751363c5c1839c525e9aaca
Rui Gomes <ruigomeseu@gmail.com>
2014-09-13 15:35:27 +0100

    Fixed share page h1 tag

--- app/views/pages/share.blade.php
+++ app/views/pages/share.blade.php
@@ -4,7 +4,7 @@
 
 <div class="error-code">{{ $sight->name }}</div>
     <br />
-    <h1>I've just checked in at {{ $sight->name }} using <a href="http://sightseeing.io">Sightseeing.io</a>!</h2>
+    <h1>I've just checked in at {{ $sight->name }} using <a href="http://sightseeing.io">Sightseeing.io</a>!</h1>
 
     <h3><img src="http://developer.android.com/images/brand/en_generic_rgb_wo_45.png"> | <img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg"></h3>
 
